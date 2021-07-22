270a15d22f7390f7e090a499c545380f5502fc70
EgorVertaev <eholife123@gmail.com>
2021-07-22 09:57:32 +0300

    fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function(){
      
-    $('.slider').slick({
+    $('.slider-discont__list').slick({
         dots: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/right.svg" alt=""></button>',
     });
 
-    var mixer = mixitup('.product__list');
+    var mixer = mixitup('.top-product__list');
 })
