7c1026aa18acb29329bc441db78a3bde5aa82010
EgorVertaev <eholife123@gmail.com>
2021-08-09 09:36:49 +0300

    added responsive and fixed mistakes

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,17 @@ $(function(){
         dots: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/icons/right.svg" alt=""></button>',
+
+        responsive: [
+            {
+              breakpoint: 1600,
+              settings: {
+                    arrows: false,
+                    dots: true,
+              }  
+                
+            }
+        ]
     });
 
     $('.brands__list').slick({
@@ -13,6 +24,36 @@ $(function(){
         slidesToShow: 6,
         slidesToScroll: 1,
 
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 
     window.onscroll = function showHeader() {
@@ -35,6 +76,42 @@ $(function(){
         $('.basket').removeClass('basket--show');
     });
     
+
+    $('.burger-menu').on('click', function(event) {
+        event.preventDefault();
+        $('.menu-mob').addClass('menu-mob--show');
+    });
+    
+    $('.menu-mob__btn').on('click', function(event) {
+        event.preventDefault();
+        $('.menu-mob').removeClass('menu-mob--show');
+    });
+
+    $('.header__user-item--search').on('click', function(event) {
+        event.preventDefault();
+        $('.header__search').addClass('header__search--show');
+    });
+    
+    $('.header__btn-search').on('click', function(event) {
+        event.preventDefault();
+        $('.header__search').removeClass('header__search--show');
+    });
+
+    $(window).on('load resize', function() {
+        if ($(window).width() < 992) {
+            $('input[type="search"]').attr('placeholder', 'Я ищу ...');
+        } else {
+            $('input[type="search"]').attr('placeholder', 'Найти в магазине ...');
+        }
+    });
+
+    $(document).on('click', function(e) {
+        if (!$(e.target).hasClass('header__search')){
+            $('.header__search').removeClass('header__search--show');
+        }
+    });
+    
+
     let containerEl1 = document.querySelector ('.top-product');
     let containerEl2 = document.querySelector ('.promo');
     
