76e60877103f4ccc38c8053ee60c65ef5bac793d
EgorVertaev <eholife123@gmail.com>
2021-09-15 21:51:51 +0300

    added catalog page

--- app/js/main.js
+++ app/js/main.js
@@ -19,7 +19,7 @@ $(function(){
 
     $('.brands__list').slick({
         arrows: false,
-        dots: true,
+        dots: false,
         infinite: false,
         slidesToShow: 6,
         slidesToScroll: 1,
@@ -56,6 +56,58 @@ $(function(){
         ]
     });
 
+    $('.catalog__select--style').styler({
+
+    })
+
+    //filter-btn 
+    $('.catalog__main-btn--filter').on('click', function() {
+      $('body').addClass('overlay');
+      $('.catalog__filters').addClass('catalog__filters--show')
+    })
+
+    $('.wrapper').on("click", function(e) {
+      $('body').removeClass('overlay');
+      $(".catalog__filters").removeClass("catalog__filters--show"); 
+    });
+
+    $('.catalog__filters-close').on('click', function(){
+      $('body').removeClass('overlay');
+      $(".catalog__filters").removeClass("catalog__filters--show"); 
+    })
+
+    // select-catalog 
+
+    $('.jq-selectbox__select').on('click', function() {
+      $(this).toggleClass('jq-selectbox__select--active')
+    })
+   
+
+    // shop-btn 
+    $('.catalog__main-btn--list, .catalog__main-btn--grid').on('click', function () {
+      $('.catalog__main-btn').removeClass('catalog__main-btn--active');
+      $(this).addClass('catalog__main-btn--active');
+    });
+
+    //shop filter (list/grid)
+    $('.catalog__main-btn--list').on('click', function() {
+      $('.product-card').addClass('product-card--list');
+      $('.catalog__content').addClass('catalog__content--list')
+    })
+
+    $('.catalog__main-btn--grid').on('click', function() {
+      $('.product-card').removeClass('product-card--list');
+      $('.catalog__content').removeClass('catalog__content--list')
+    })
+
+    $(window).on('load resize', function() {
+      if($(window).width() < 1200) {
+        $('.product-card').removeClass('product-card--list')
+        $('.catalog__content').removeClass('catalog__content--list')
+      }
+    });
+
+    // scroll header
     window.onscroll = function showHeader() {
         let header = document.querySelector('.header__bottom')
 
@@ -65,11 +117,13 @@ $(function(){
             header.classList.remove('header__bottom--scroll')
         }
     };
-
+    // basket
     $('.header__user-link--card').on('click', function(event) {
-        event.preventDefault();
+        if ($(window).width()> 576) {
+          $('body').addClass('overlay');
+        };
         $('.basket').addClass('basket--show');
-        $('body').addClass('overlay');
+
     });
     
     $('.btn-close').on('click', function(event) {
@@ -82,7 +136,8 @@ $(function(){
           $('body').removeClass('overlay');
           $(".basket").removeClass("basket--show"); 
       });
-
+    
+      //burger-menu
     $('.burger-menu').on('click', function(event) {
         event.preventDefault();   
         $('.menu-mob').addClass('menu-mob--show');
@@ -107,7 +162,7 @@ $(function(){
 
     $('.header__user-link--search').on('click', function(event) {
         event.preventDefault();
-        $('.header__search').addClass('header__search--showw');
+        $('.header__search').toggleClass('header__search--showw');
     });
     
     $('.header__btn-search').on('click', function(event) {
@@ -125,6 +180,79 @@ $(function(){
         }
       });
 
+    // catalog dropdown
+    $('.header__catalog').hover(() => {
+      if ($(window).width() > 992) {
+      $('.header__catalog-list').slideToggle(350);
+      }
+    })  
+
+    // dropdown - catalog-page
+    $('.catalog__box-title').click(function(event) { 
+      $(this).toggleClass('catalog__box-title--active').next().slideToggle(300);
+    })
+
+    // range-slider 
+    var $range = $(".catalog__input-range"),
+    $inputFrom = $(".catalog__input-from"),
+    $inputTo = $(".catalog__input-to"),
+    instance,
+    min = 0,
+    max = 1000,
+    from = 0,
+    to = 0;
+
+    $range.ionRangeSlider({
+        type: "double",
+        min: min,
+        max: max,
+        from: 0,
+        to: 1000,
+        onStart: updateInputs,
+        onChange: updateInputs
+    });
+    instance = $range.data("ionRangeSlider");
+
+    function updateInputs (data) {
+      from = data.from;
+        to = data.to;
+        
+        $inputFrom.prop("value", from);
+        $inputTo.prop("value", to);	
+    }
+
+    $inputFrom.on("input", function () {
+        var val = $(this).prop("value");
+        
+        // validate
+        if (val < min) {
+            val = min;
+        } else if (val > to) {
+            val = to;
+        }
+        
+        instance.update({
+            from: val
+        });
+    });
+
+    $inputTo.on("input", function () {
+        var val = $(this).prop("value");
+        
+        if (val < from) {
+            val = from;
+        } else if (val > max) {
+            val = max;
+        }
+        
+        instance.update({
+            to: val
+        });
+    });
+
+
+
+    // Search atribyte
     $(window).on('load resize', function() {
         if ($(window).width() < 992) {
             $('input[type="search"]').attr('placeholder', 'Я ищу ...');
@@ -133,16 +261,24 @@ $(function(){
         }
     });
 
+    // Atribyte logo
+    $(window).on('load resize', function() {
+      if ($(window).width() < 576) {
+          $('.header__logo-img').attr('width', '40');
+          $('.header__logo-img').attr('height', '40');
+      } else {
+          $('.header__logo-img').attr('width', '200');
+          $('.header__logo-img').attr('height', '60');
+      }
+  });
+  // search btn
     $(document).on('click', function(e) {
         if (!$(e.target).hasClass('header__search')){
             $('.header__search').removeClass('header__search--show');
         }
     });
-    
-    $('.header__catalog').hover(() => {
-      $('.header__catalog-list').slideToggle(350);
-    })  
-
+   
+    // basket + - 
     $('.basket-card__btn--minus').on("click", function(e) {
       let count = $(this).parent().children('.basket-card__num').val()
       if (+count > 1) {
@@ -154,8 +290,12 @@ $(function(){
       let count = $(this).parent().children('.basket-card__num').val()
       $(this).parent().children('.basket-card__num').val(+count + 1);
     });
+
+    $('.pagination__link--noclick').on("click", function(e) {
+        event.preventDefault(); 
+    })
     
-   
+  //  mixitup local 
     let containerEl1 = document.querySelector ('.top-product');
     let containerEl2 = document.querySelector ('.promo');
     
--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,10 @@ const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 const fileinclude = require('gulp-file-include');
-const sprite = require('gulp-svg-sprite');
+const sprite = require('gulp-svg-sprite'),
+      cheerio = require('gulp-cheerio'),
+	  replace = require('gulp-replace');
+
 
 function browser() {
     browserSync.init ({
@@ -36,6 +39,8 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
@@ -56,6 +61,16 @@ function img() {
 
 function svgSprite() {
     return src('app/images/sprite/*.svg')
+
+    .pipe(cheerio({
+        run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('[style]').removeAttr('style');
+        },
+        parserOptions: {xmlMode: true}
+    }))
+    .pipe(replace('&gt;', '>'))
     .pipe(sprite({
         mode: {
             stack: {
@@ -88,7 +103,7 @@ function images() {
             ]
         })
     ]))
-    .pipe(dest('dist/images'))
+    .pipe(dest('build/images'))
 }
 
 function build() {
@@ -97,17 +112,17 @@ function build() {
         'app/css/style.min.css',
         'app/js/main.min.js'
     ], {base: 'app'})
-    .pipe(dest('dist'))
+    .pipe(dest('build'))
 }
 
-function cleanDist() {
-    return del('dist')
+function cleanBuild() {
+    return del('build')
 }
 
 function watching() { 
     watch(['app/scss/**/*.scss'], styles);
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/*.html'], html)
+    watch(['app/**/*.html'], html)
     watch('app/images/content/*', parallel('images'));
     watch('app/images/sprite/*', parallel('svgSprite'));
     watch('app/fonts/*', parallel('fonts'));
@@ -118,13 +133,12 @@ exports.scripts = scripts;
 exports.browser = browser;
 exports.watching = watching;
 exports.images = images;
-exports.cleanDist = cleanDist;
+exports.cleanBuild = cleanBuild;
 exports.svgSprite = svgSprite;
 exports.html = html;
 exports.img = img;
 exports.fonts = fonts;
 
-exports.build = series(cleanDist, images, build);
-
+exports.build = series(cleanBuild, images, build);
 exports.default = series(parallel(styles, scripts, fonts, img, html, images, svgSprite), parallel(browser, watching));
 
